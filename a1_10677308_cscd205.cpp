4d7dce3f1cd3a468ea6455dad67d9682929a5577
Dzidzienyo <42819054+Dzidzienyo@users.noreply.github.com>
2018-08-29 22:59:46 +0000

    Add files via upload

---
+++ a1_10677308_cscd205.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i , c = 0;
+    cout << "Enter any number n:" << endl;
+    cin >> n;
+    {
+        if (n% i==0)
+        {
+
+            c++;
+        }
+    }
+    if (c==2)
+    {
+        cout<<"n is not prime number"<<endl;
+    }
+        else
+        {
+            cout<<"n is a prime number"<<endl;
+        }
+
+    return 0;
+}
+
